e376ad3f24e15f122bb088d316e3c847f984e5df
kurnia22 <kurniawati.22.nk@gmail.com>
2017-09-18 13:16:15 +0700

    refactor code

---
+++ src/components/lib/util.js
@@ -0,0 +1,1 @@
+export const BASE_API_URL = 'http://172.104.50.9:3000/api'
--- src/components/product/product.js
+++ src/components/product/product.js
@@ -4,9 +4,10 @@ import {
   Link
 } from 'react-router-dom';
 import axios from 'axios';
+import {BASE_API_URL} from '../lib/util';
 
 
-const base_URL = 'http://172.104.50.9:3000/api'
+
   		  
 
 
@@ -26,7 +27,7 @@ class Product extends Component{
 
     getProduct = async () =>{
         try{
-            let response = await axios.get(base_URL +'/shoppinglists')
+            let response = await axios.get(BASE_API_URL +'/shoppinglists')
             let responseJson = await response;
             console.log(responseJson)
             this.setState({data_product_api:response.data})
@@ -36,43 +37,62 @@ class Product extends Component{
         }
     }
 
-       render(){
+    render(){
+        if(this.state.data_product_api.length>0){
+            return <Product_thumbnail data={this.state.data_product_api}/>
+        }
+        return <div>Loading...</div>
 
-           console.log(this.state.data_product_api)
-           const product_thumbnail = this.state.data_product_api.map((res, index)=>{
-               return(
-                   <div className="col-sm-3" key={index}>
-                        
-                        <div className="card product">
-                            <img className="card-img-top" src={res.image} alt="Product" ></img>
-                            <div className="card-block">
-                                <Link to="/pdp">
-                                <h4 className="card-title">{res.product_name}</h4>
-                                <p className="card-text">Rp {res.price}</p>
-                                </Link>
-                                <a href="#" className="btn btn-primary">Add to Cart </a>
-                                <br/><br/>
-                                 
-                            </div>
-                        </div>
-                    </div>
-                   
-               )
-           })
+    }  
+ 
+    
+}            
 
-            return(
-                <div className="container">
-                    <div className="row">
-                        {product_thumbnail}
+
+
+
+const Product_thumbnail =(props)=>{
+    const Products = props.data.map((res, index)=>{
+        return(
+            <div className="col-sm-3" key={index}>
+                <div className="card product">
+                    <img className="card-img-top" src={res.image} alt="Product" ></img>
+                    <div className="card-block">
+                        <Link to="/pdp">
+                        <h4 className="card-title">{res.product_name}</h4>
+                        <p className="card-text">Rp {res.price}</p>
+                        </Link>
+                        <a href="#" className="btn btn-primary">Add to Cart </a>
+                        <br/><br/> 
                     </div>
                 </div>
+            </div>
 
-            );
+        )
+    })
+        
+    
+    return(
+        <div className="container">
+            <div className="row">
+                {Products}
+            </div>
+            <br/><br/><br/><br/><br/>
+        </div>
 
-        }
+    )
+}
 
-    }            
 
+
+
+
+
+
+
+
+    
+        
 
 
 
